337b29d4aa89854932a8997d4528264c7c5790a4
Ilyas chaoui <144786151+ichaoui56@users.noreply.github.com>
2024-12-30 17:51:04 +0100

    add 3 components needed in my app (employee-list -> parent) (employee-card-> child) (employee-form-> form)

--- src/app/features/employees/employees.module.ts
+++ src/app/features/employees/employees.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeesComponent } from './employees.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './pages/employee-form/employee-form.component';
+import { EmployeeCardComponent } from './components/employee-card/employee-card.component';
 
 
 @NgModule({
   declarations: [
-    EmployeesComponent
+    EmployeesComponent,
+    EmployeeListComponent,
+    EmployeeFormComponent,
+    EmployeeCardComponent
   ],
   imports: [
     CommonModule,

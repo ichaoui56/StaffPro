4e89408c5970d55a1db7480203d498cc4792b07b
Ilyas chaoui <144786151+ichaoui56@users.noreply.github.com>
2024-12-31 11:01:10 +0100

    add hhighlight directive and formatDate pipe in shared module

---
+++ src/app/shared/pipes/format-date.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatDate',
+  standalone: false
+})
+export class FormatDatePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HighlightDirective } from './directives/highlight.directive';
+import { FormatDatePipe } from './pipes/format-date.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HighlightDirective,
+    FormatDatePipe
+  ],
   imports: [
     CommonModule
   ]

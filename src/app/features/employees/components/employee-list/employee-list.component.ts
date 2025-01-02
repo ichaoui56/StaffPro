534b8d3e3e43dd33c89da78341e3df4ea08eb71b
Ilyas chaoui <144786151+ichaoui56@users.noreply.github.com>
2025-01-02 09:26:36 +0100

    use observables and subsc to employee actions

--- src/app/features/employees/components/employee-list/employee-list.component.ts
+++ src/app/features/employees/components/employee-list/employee-list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Employee } from '../../models/employee.model';
 import { EmployeeService } from '../../services/employee.service';
 import Swal from 'sweetalert2';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
-  standalone:false,
+  standalone: false,
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit, OnDestroy {
   employees: Employee[] = [];
+  private destroy$ = new Subject<void>();
 
   constructor(
     private employeeService: EmployeeService,
@@ -19,9 +21,48 @@ export class EmployeeListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.employeeService.getEmployeesObservable().subscribe(employees => {
-      this.employees = employees;
-    });
+    // Subscribe to employees list updates
+    this.employeeService.employees$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(employees => {
+        this.employees = employees;
+      });
+
+    // Subscribe to employee actions for notifications
+    this.employeeService.employeeAction$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(({ action, employee }) => {
+        let message = '';
+        switch (action) {
+          case 'deleted':
+            message = `${employee.firstName} ${employee.lastName} has been deleted`;
+            break;
+          case 'updated':
+            message = `${employee.firstName} ${employee.lastName}'s information has been updated`;
+            break;
+          case 'added':
+            message = `${employee.firstName} ${employee.lastName} has been added`;
+            break;
+        }
+        
+        if (message) {
+          Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            text: message,
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true
+          });
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   addNewEmployee(): void {
@@ -30,15 +71,5 @@ export class EmployeeListComponent implements OnInit {
 
   handleDeleteEmployee(id: string): void {
     this.employeeService.deleteEmployee(id);
-    Swal.fire({
-      icon: 'success',
-      title: 'Deleted!',
-      text: 'Employee has been deleted.',
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true
-    });
   }
 }
--- src/app/features/employees/services/employee.service.ts
+++ src/app/features/employees/services/employee.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { Employee } from '../models/employee.model';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -9,6 +9,10 @@ import { v4 as uuidv4 } from 'uuid';
 export class EmployeeService {
   private readonly STORAGE_KEY = 'employees';
   private employeesSubject = new BehaviorSubject<Employee[]>(this.loadEmployees());
+  private employeeActionSubject = new Subject<{action: string, employee: Employee}>();
+
+  employeeAction$ = this.employeeActionSubject.asObservable();
+  employees$ = this.employeesSubject.asObservable();
 
   addEmployee(employee: Employee): void {
     const employees = this.getEmployees();
@@ -19,31 +23,7 @@ export class EmployeeService {
     employees.push(employeeWithId);
     localStorage.setItem(this.STORAGE_KEY, JSON.stringify(employees));
     this.employeesSubject.next(employees);
-  }
-
-  getEmployees(): Employee[] {
-    return this.loadEmployees();
-  }
-
-  getEmployeesObservable(): Observable<Employee[]> {
-    return this.employeesSubject.asObservable();
-  }
-
-  private loadEmployees(): Employee[] {
-    const employees = localStorage.getItem(this.STORAGE_KEY);
-    return employees ? JSON.parse(employees) : [];
-  }
-
-  deleteEmployee(id: string): void {
-    const employees = this.getEmployees();
-    const filteredEmployees = employees.filter(employee => employee.id !== id);
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(filteredEmployees));
-    this.employeesSubject.next(filteredEmployees);
-  }
-
-  getEmployeeById(id: string): Employee | undefined {
-    const employees = this.getEmployees();
-    return employees.find(emp => emp.id === id);
+    this.employeeActionSubject.next({ action: 'added', employee: employeeWithId });
   }
 
   updateEmployee(updatedEmployee: Employee): void {
@@ -53,6 +33,32 @@ export class EmployeeService {
       employees[index] = updatedEmployee;
       localStorage.setItem(this.STORAGE_KEY, JSON.stringify(employees));
       this.employeesSubject.next(employees);
+      this.employeeActionSubject.next({ action: 'updated', employee: updatedEmployee });
     }
+  }
+
+  deleteEmployee(id: string): void {
+    const employees = this.getEmployees();
+    const employeeToDelete = employees.find(emp => emp.id === id);
+    if (employeeToDelete) {
+      const filteredEmployees = employees.filter(employee => employee.id !== id);
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(filteredEmployees));
+      this.employeesSubject.next(filteredEmployees);
+      this.employeeActionSubject.next({ action: 'deleted', employee: employeeToDelete });
+    }
+  }
+
+  getEmployees(): Employee[] {
+    return this.loadEmployees();
+  }
+
+  getEmployeeById(id: string): Employee | undefined {
+    const employees = this.getEmployees();
+    return employees.find(emp => emp.id === id);
+  }
+
+  private loadEmployees(): Employee[] {
+    const employees = localStorage.getItem(this.STORAGE_KEY);
+    return employees ? JSON.parse(employees) : [];
   }
 }
